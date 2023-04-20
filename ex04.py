db13ff2dc68245dad5b637b106f6a709d63f3d03
gabbiankati <128652887+gabbiankati@users.noreply.github.com>
2023-04-19 22:45:05 -0300

    Add files via upload

---
+++ ex04.py
@@ -0,0 +1,9 @@
+larg = float(input('Informe a largura do pacote: '))
+alt = float(input('Informe a altura do pacote: '))
+comp = float(input('Informe o comprimento do pacote: '))
+
+resultado = (4 * alt) + (2 * larg) + (2 * comp) + 20
+
+print(f'O total de barbante a ser usado é {resultado:.2f} ')
+
+
